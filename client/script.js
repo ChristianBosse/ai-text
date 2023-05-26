5eb95c9fb0988cc00bfc3dba928223e7a1953e0a
Christian Bosse <christianbosse1998@gmail.com>
2023-05-26 16:59:11 -0300

    added backend to deploiement

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
 
     //fetch data from server => bot response
 
-    const response = await fetch("http://localhost:5000", {
+    const response = await fetch("https://chrisgpt.onrender.com", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
